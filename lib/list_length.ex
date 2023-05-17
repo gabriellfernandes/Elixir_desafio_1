0f88e291e6bd36b395e33349cd33a1baec402d7d
gabriellfernandes <gabriellf.dev@gmail.com>
2023-05-17 16:44:57 -0300

    concluded

--- lib/list_length.ex
+++ lib/list_length.ex
@@ -1,5 +1,14 @@
 defmodule ListLength do
-  def call() do
-    #TO DO
+  #Utilizando modulo para verificar o tamanho da lista;
+  #def call(list), do: length(list)
+
+  #Sem utilizar o módulo length e utilizando recursividade;
+  def call(list), do: list_length(list, 0)
+
+  defp list_length([], length), do: length
+
+  defp list_length([ _head | tail], length) do
+    length = length + 1
+    list_length(tail, length)
   end
 end
